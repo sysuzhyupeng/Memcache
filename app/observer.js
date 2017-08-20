1120f0090144857a3f7aadef8d97265799fa5e0a
sysuzhyupeng <673341297@qq.com>
2017-08-20 10:07:18 +0800

    observer.js

--- app/observer.js
+++ app/observer.js
@@ -19,12 +19,45 @@ document.body.addEventListener('click', function(){
 document.body.addEventListener('click', function(){
 	alert(3);
 }, false);
-document.body.click();
 /*
 	注意，手动触发事件更好的做法是IE下用fireEvent，标准浏览器下用dispatchEvent
 */
+document.body.click();
 /*
 	自定义事件
-
 	比如我们实现一个天气预报的广播功能
+*/
+var weatherCenter1 = {};
+/*
+	缓存列表，存放订阅者的回调函数
+	当listen的时候，实际上是把订阅者的回调fn放进数组中
+*/
+weatherCenter1.clientList = [];
+weatherCenter1.listen = function(fn){
+  	this.clientList.push(fn);
+}
+weatherCenter1.trigger = function(){
+	for(var i = 0, fn; i < this.clientList.length; i++){
+	    fn = this.clientList[i];
+	    //trigger的时候用apply改变为trigger时的参数
+	    fn.apply(this, arguments);
+	}
+}
+//下面我们进行一些简单的测试
+//A订阅消息(自定义回调函数，也就是自定义订阅消息的推送格式)
+weatherCenter1.listen(function(type, temprature){
+   console.log('温度=' + temprature);
+   console.log('天气类型为' + type);
+})
+//B订阅消息b 
+weatherCenter1.listen(function(type, temprature){
+   console.log('温度=' + temprature);
+   console.log('天气类型为' + type);
+})   
+weatherCenter1.trigger('rainy', '12');
+weatherCenter1.trigger('sunny', '30');
+/*
+	至此，我们实现了简单的发布-订阅模式，
+	但这里还有一点问题，假如A只想知道晴天的天气推送，
+	那么我们有必要增加一个标识key，让订阅者只订阅自己感兴趣的消息
 */
