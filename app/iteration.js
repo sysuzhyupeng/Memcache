856c4ea22ad8307654c2695ed481cd79919cdf4c
sysuzhyupeng <673341297@qq.com>
2017-08-24 09:38:24 +0800

    Iterator

---
+++ app/iteration.js
@@ -0,0 +1,84 @@
+/*
+	迭代器模式是指通过一种方法顺序访问一个聚合对象的各个元素，
+	而又不需要暴露该对象的内部表示，
+	在使用迭代器模式之后，
+	即使不关心对象的内部构造，也可以按顺序访问其中的每个元素
+*/
+/*
+	现在我们自己实现一个each函数，
+	接受两个参数，
+	第一个为被循环的数组，
+	第二个为循环中的每一步将被触发的回调函数
+*/
+var each = function(arr, callback){
+	for(var i = 0, len = arr.length; i < len; i++){
+		//把下标和元素，当做参数传给callback函数
+		callback.call(arr[i], i, arr[i]);
+	}
+}
+each([1, 2, 3], function(k, v){
+	console.log('each', k, v);
+});
+/*
+   each属于内部迭代器，
+   each函数的内部已经定义好了迭代规则，
+   外界不用关心迭代器的实现，跟迭代器的交互也仅仅是一次初始调用，
+   但如果现在有个需求，要判断2个数组元素的值是否相等，
+   如果不改写each函数的代码，只能在callback中实现
+*/
+var compare = function(arr1, arr2){
+	var len1 = arr1.length,
+		len2 = arr2.length;
+	if(len1 !== len2){
+		throw new Error('arr1和arr2不相等');
+	}
+	each(arr1, function(k, v){
+		if(v !== arr2[k]){
+			throw new Error('arr1和arr2不相等');
+		}
+	});
+	console.log('compare', 'arr1和arr2相等');
+}
+compare([1, 2, 3], [1, 2, 3]);
+/*
+	外部迭代器必须显示地请求下一个迭代元素，
+	增加了一些调用的复杂度，
+	但相对也增强了迭代器的灵活性
+*/
+var Iterator = function(obj){
+	var current = 0;
+
+	var next = function(){
+		current++;
+	}
+	var isDone = function(){
+		return current >= obj.length;
+	}
+	var getCurrItem = function(){
+		return obj[current];
+	}
+	return {
+		next: next,
+		isDone: isDone,
+		getCurrItem: getCurrItem
+	}
+}
+/*
+	再看看如何改写compare函数
+*/
+var compare2 = function(iterator1, iterator2){
+	while(!iterator1.isDone && !iterator2.isDone){
+		if(iterator1.getCurrItem() !== iterator2.getCurrItem()){
+			throw new Error('iterator1和iterator2不相等');
+		}
+		iterator1.next();
+		iterator2.next();
+	}
+	console.log('compare2', 'iterator1和iterator1相等');
+}
+var iterator1 = Iterator([1, 2, 3]),
+	iterator2 = Iterator([1, 2, 3]);
+compare2(iterator1, iterator2);
+/*
+	
+*/
--- app/observer.js
+++ app/observer.js
@@ -5,7 +5,7 @@
 	实际上我们在DOM节点上绑定事件处理函数，就是一种发布-订阅模式
 */
 document.body.addEventListener('click', function(){
-	alert(1);
+	console.log(1);
 }, false);
 /*
 	当loginBtn1节点被点击时，
@@ -14,10 +14,10 @@ document.body.addEventListener('click', function(){
 	增加任何订阅者都不会影响发布者代码的编写
 */
 document.body.addEventListener('click', function(){
-	alert(2);
+	console.log(2);
 }, false);
 document.body.addEventListener('click', function(){
-	alert(3);
+	console.log(3);
 }, false);
 /*
 	注意，手动触发事件更好的做法是IE下用fireEvent，标准浏览器下用dispatchEvent
@@ -46,18 +46,74 @@ weatherCenter1.trigger = function(){
 //下面我们进行一些简单的测试
 //A订阅消息(自定义回调函数，也就是自定义订阅消息的推送格式)
 weatherCenter1.listen(function(type, temprature){
-   console.log('温度=' + temprature);
+   console.log('温度=' + temprature, 'from weatherCenter1');
    console.log('天气类型为' + type);
 })
 //B订阅消息b 
 weatherCenter1.listen(function(type, temprature){
-   console.log('温度=' + temprature);
+   console.log('温度=' + temprature, 'from weatherCenter1');
    console.log('天气类型为' + type);
 })   
-weatherCenter1.trigger('rainy', '12');
-weatherCenter1.trigger('sunny', '30');
+// weatherCenter1.trigger('rainy', '12');
+// weatherCenter1.trigger('sunny', '30');
 /*
 	至此，我们实现了简单的发布-订阅模式，
 	但这里还有一点问题，假如A只想知道晴天的天气推送，
 	那么我们有必要增加一个标识key，让订阅者只订阅自己感兴趣的消息
 */
+var weatherCenter2 = {};
+weatherCenter2.clientList = {};
+weatherCenter2.listen = function(key, fn){
+    if(!this.clientList[key]){
+        this.clientList[key] = [];
+    }
+    this.clientList[key].push(fn);
+}
+weatherCenter2.trigger = function(){
+    var key = Array.prototype.shift.call(arguments),
+        //取出该消息对应的回调函数集合
+        fns = this.clientList[key];
+    if(!fns || fns.length === 0){
+        return false;
+    }
+    for(var i = 0; i < fns.length; i++){
+    	fn = fns[i];
+        //arguments对象的第一个参数已经被shift
+        fn.apply(this, arguments);
+    }
+}
+weatherCenter2.listen('sunny', function(temprature){
+    console.log('温度=' + temprature, 'from weatherCenter2');
+})
+weatherCenter2.listen('rainy', function(temprature){
+    console.log('温度=' + temprature, 'from weatherCenter2');
+})
+weatherCenter2.trigger('rainy', '14');
+weatherCenter2.trigger('sunny', '30');
+/*
+	发布-订阅模式的通用实现
+	我们假设小明又去其他气象中心订阅了天气，
+	那这段代码是否应该在另一个气象中心上重写一次呢，
+	有没有办法可以让所有对象都拥有发布-订阅功能呢 
+	所以我们把发布-订阅的功能提取出来，放在一个单独的对象内
+*/
+var event = {
+      clientList = [],
+      listen: function(key, fn){
+         if(!this.clientList[key]){
+            this.clientList[key] = [];
+         }
+         this.clientList[key].push(fn);
+      },
+      trigger: function(){
+         var key = Array.prototype.shift.call(arguments),
+           fns = this.clientList(key);
+         if(!fn || fn.length === 0){
+            return false;
+         }
+         for(var i = 0, fn; fn = fns[i++]){
+            //arguments对象的第一个参数已经被shift
+            fn.apply(this, arguments);
+         }
+      }
+   }
